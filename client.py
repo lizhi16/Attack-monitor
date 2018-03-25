274fbe4256f5c981ab8c4aabe0a49da006372eec
lizhi16 <791873122@qq.com>
2018-03-25 10:47:12 +0800

    Update client.py

--- client.py
+++ client.py
@@ -14,11 +14,11 @@ import sys
 def socket_client():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('***** ip of server*****', 6666))
+        s.connect((sys.argv[1], 6666))
     except socket.error as msg:
         print msg
         sys.exit(1)
-    data = sys.argv[1]
+    data = sys.argv[2]
     s.send(data)
     s.close()
 
